baec1a16ad0b3c9fd9a185dc596a6118ae8290bd
liuzihau <pokerliuzihau@gmail.com>
2024-12-20 17:26:58 +1100

    change import_utils->_get_module, remove the "." in front of the module name

--- utils/import_utils.py
+++ utils/import_utils.py
@@ -1794,7 +1794,8 @@ class _LazyModule(ModuleType):
 
     def _get_module(self, module_name: str):
         try:
-            return importlib.import_module("." + module_name, self.__name__)
+            # return importlib.import_module("." + module_name, self.__name__)
+            return importlib.import_module(module_name, self.__name__)
         except Exception as e:
             raise RuntimeError(
                 f"Failed to import {self.__name__}.{module_name} because of the following error (look up to see its"
